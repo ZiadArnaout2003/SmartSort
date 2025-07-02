477d0d016943a053ac166cfacff28bfa1c1500c0
Aboubacry Ba <abou9710@gmail.com>
2025-07-01 21:11:24 -0400

    Program to classify objects

--- app/src/main/java/ca/concordia/smartsortandroidapp/MainActivity.java
+++ app/src/main/java/ca/concordia/smartsortandroidapp/MainActivity.java
@@ -1,24 +1,48 @@
 package ca.concordia.smartsortandroidapp;
 
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView labelTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        labelTextView = findViewById(R.id.label_text); 
+
+        try {
+            ImageClassifier classifier = new ImageClassifier(getAssets());
+
+            StringBuilder results = new StringBuilder();
+            AssetManager assetManager = getAssets();
+            String[] imageFiles = assetManager.list("test_pictures"); 
+
+            if (imageFiles != null) {
+                for (String filename : imageFiles) {
+                    try {
+                        InputStream is = assetManager.open("test_pictures/" + filename);
+                        Bitmap bitmap = BitmapFactory.decodeStream(is);
+                        String label = classifier.classify(bitmap);
+                        results.append(filename).append(" -> ").append(label).append("\n");
+                        is.close();
+                    } catch (Exception ignored) {}
+                }
+            }
+
+            labelTextView.setText(results.toString());
+
+        } catch (Exception e) {
+            labelTextView.setText("Error: " + e.getMessage());
+        }
     }
 }
